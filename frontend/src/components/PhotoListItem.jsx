0a70b69c9ba0303528501d39843304eebfb90ff9
Mariam Agboola <mariahhm42@gmail.com>
2025-03-23 23:08:25 -0600

    edited how properties of the photo prop gets access in PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import "../styles/PhotoListItem.scss";
 
 
-const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+const PhotoListItem = ({ photo }) => {
+  const { id, location, imageSource, username, profile } = photo;
+
   return (
-    <div className="photo-list-item" key={id}>
-      {/* User Profile Section */}
-      <div className="photo-list-item__header">
-        <img src={profile} alt={`${username}'s profile`} className="photo-list-item__profile" />
-        <h2 className="photo-list-item__username">{username}</h2>
+    <div className="photo-list-item">
+      <img src={imageSource} alt={`Photo ${id}`} />
+      <div className="photo-info">
+        <img src={profile} alt={`${username}'s profile`} className="profile-pic" />
+        <p>{username}</p>
+        <p>{location.city}, {location.country}</p>
       </div>
-
-      {/* Main Photo */}
-      <img src={imageSource} alt="Photo" className="photo-list-item__image" />
-
-      {/* Location */}
-      <p className="photo-list-item__location">{`${location.city}, ${location.country}`}</p>
     </div>
   );
 };
