139f95f238ea52f752c61af9f412f31fe8ce5c85
Mariam Agboola <mariahhm42@gmail.com>
2025-03-23 22:30:08 -0600

    moved the sampleDataForPhotoListItem object into App.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,24 +1,23 @@
+import React from 'react';
 import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
-  },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
-  },
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+  return (
+    <div className="photo-list-item" key={id}>
+      {/* User Profile Section */}
+      <div className="photo-list-item__header">
+        <img src={profile} alt={`${username}'s profile`} className="photo-list-item__profile" />
+        <h2 className="photo-list-item__username">{username}</h2>
+      </div>
+
+      {/* Main Photo */}
+      <img src={imageSource} alt="Photo" className="photo-list-item__image" />
+
+      {/* Location */}
+      <p className="photo-list-item__location">{`${location.city}, ${location.country}`}</p>
+    </div>
+  );
 };
 
 export default PhotoListItem;
