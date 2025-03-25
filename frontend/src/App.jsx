b8342c644019db9e98abc1f0e8232f751cb227fe
Mariam Agboola <mariahhm42@gmail.com>
2025-03-24 21:14:37 -0600

    referenced images properly in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,24 +9,16 @@ const sampleDataForPhotoListItem = {
     city: "Montreal",
     country: "Canada",
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  imageSource: `/Image-1-Regular.jpeg`,
   username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  profile: `$/profile-1.jpg`,
 };
 
 // Note: Rendering a single component to build components in isolation
-const App = () => {
-  return (
-    <div className="App">
-      <PhotoListItem
-      id={sampleDataForPhotoListItem.id}
-      location={sampleDataForPhotoListItem.location}
-      imageSource={sampleDataForPhotoListItem.imageSource}
-      username={sampleDataForPhotoListItem.username}
-      profile={sampleDataForPhotoListItem.profile}
-      />
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <PhotoListItem photo={sampleDataForPhotoListItem} />
+  </div>
+);
 
 export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import "../styles/PhotoListItem.scss";
 
-
 const PhotoListItem = ({ photo }) => {
   const { id, location, imageSource, username, profile } = photo;
 
   return (
-    <div className="photo-list-item">
-      <img src={imageSource} alt={`Photo ${id}`} />
-      <div className="photo-info">
-        <img src={profile} alt={`${username}'s profile`} className="profile-pic" />
-        <p>{username}</p>
-        <p>{location.city}, {location.country}</p>
+    <div className="photo-list__item">
+      <img src={imageSource} alt={`Photo ${id}`} className="photo-list__image" />
+      <div className="photo-list__user-details">
+        <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
+        <div className="photo-list__user-info">
+          <p>{username}</p>
+          <div className="photo-list__user-location">{location.city}, {location.country}</div>
+        </div>
       </div>
     </div>
   );
