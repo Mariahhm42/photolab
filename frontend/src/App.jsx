748713fca49d7882e96430e76a7e97342930fcad
Mariam Agboola <mariahhm42@gmail.com>
2025-03-26 22:39:21 -0600

    mport the mock data and pass it as props to the HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import HomeRoute from './components/HomeRoute'; // Import the new HomeRoute
 import './App.scss';
 
+// Import mock data
+import photos from './mocks/photos';
+import topics from './mocks/topics';
+
+console.log("Mock Photos in App:", photos); // Debugging
+
 const App = () => (
   <div className="App">
-    <HomeRoute /> {/* Only render the HomeRoute component */}
+    <HomeRoute photos={photos} topics={topics}/> {/* Only render the HomeRoute component */}
   </div>
   );
 
