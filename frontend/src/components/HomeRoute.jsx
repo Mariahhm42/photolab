d13df7d0c305bdc76f218b7f155722ef3d84c79d
Mariam Agboola <mariahhm42@gmail.com>
2025-03-26 22:42:51 -0600

    modified to consume props from the root application Component, App.jsx, consisting of topics and photos.

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -3,15 +3,16 @@ import TopNavigationBar from './TopNavigationBar'; // Import TopNavigationBar
 import PhotoList from './PhotoList'; // Import PhotoList component (to display photos)
 import '../styles/HomeRoute.scss'; // Include styling for HomeRoute
 
-const HomeRoute = () => {
+const HomeRoute = ({ photos, topics }) => {
+    console.log("Received Photos in HomeRoute:", photos); // Debugging
   return (
     <div className="home-route">
-      {/*renders the Top Navigation Bar */}
-      <TopNavigationBar />
+      {/*Pass topic to the Top Navigation Bar */}
+      <TopNavigationBar topics={topics} />
 
-      {/* Photos Section */}
+      {/* Pass photo to PhotoList */}
       <div className="home-route__photos">
-        <PhotoList /> {/* This will display the list of photos */}
+        <PhotoList photos={photos}/> {/* This will display the list of photos */}
       </div>
     </div>
   );
