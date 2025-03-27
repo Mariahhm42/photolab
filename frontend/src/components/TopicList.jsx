2d9e846b02ac70ea7505e12035dbc1ed197ab2a9
Mariam Agboola <mariahhm42@gmail.com>
2025-03-26 22:41:25 -0600

    modified component to accept topic data as props instead of using the static

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,34 +1,36 @@
 import React from "react";
-import TopicListItem from "./TopicListItem";
+//import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: 1,
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: 2,
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: 3,
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const sampleDataForTopicList = [
+//   {
+//     id: 1,
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: 2,
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: 3,
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 /**
  * TopicList Component
  * Renders a list of topics.
  */
-const TopicList = () => {
+const TopicList = ({ topics }) => {
   return (
-    <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topic) => (
-        <TopicListItem key={topic.id} topic={topic} />
+    <div className="topic-list">
+      {topics.map((topic) => (
+        <button key={topic.id} className="topic-list__item">
+          {topic.title}
+        </button>
       ))}
     </div>
   );
