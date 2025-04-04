38321553f53d8e82356cf06620df7fea06656171
Mariam Agboola <mariahhm42@gmail.com>
2025-04-04 03:36:22 -0600

    ensure that the modal in PhotoDetailsModal.jsx can be closed by using the onClose function

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,13 +1,18 @@
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({ photo }) => {
+const PhotoDetailsModal = ({ photo, onClose }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      {/* Close button that triggers the onClose function when clicked */}
+      <button 
+        className="photo-details-modal__close-button"
+        onClick={onClose} // Trigger the close action
+      >
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      {/* You can display more photo details here in the future */}
+      
+      {/* Modal content */}
       <div className="photo-details-modal__content">
         <img src={photo.urls.regular} alt={photo.alt_description} />
         <p>{photo.description}</p>
